27e4d2827b57668336e5ddcec8b47f1324c32f4b
Dylan van den Brink <dylanvandenbrink@live.nl>
2019-09-03 00:04:02 +0200

    Configured the quartz job

--- netflixhistory-watcher/src/main/java/nl/dylanvdbrink/flixtraktor/netflixhistorywatcher/NetflixhistoryWatcherApplication.java
+++ netflixhistory-watcher/src/main/java/nl/dylanvdbrink/flixtraktor/netflixhistorywatcher/NetflixhistoryWatcherApplication.java
@@ -1,13 +1,34 @@
 package nl.dylanvdbrink.flixtraktor.netflixhistorywatcher;
 
+import nl.dylanvdbrink.flixtraktor.netflixhistorywatcher.jobs.QueueViewingActivityJob;
+import org.quartz.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NetflixhistoryWatcherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetflixhistoryWatcherApplication.class, args);
+    }
+
+    @Bean
+    public JobDetail netflixViewingHistoryPersistJobDetail() {
+        return JobBuilder.newJob(QueueViewingActivityJob.class).withIdentity("queueViewingActivity")
+                .usingJobData("name", "World").storeDurably().build();
+    }
+
+    @Bean
+    public Trigger netflixViewingHistoryPersistJobTrigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInHours(1)
+                .repeatForever();
+
+        return TriggerBuilder.newTrigger()
+                .forJob(netflixViewingHistoryPersistJobDetail())
+                .withIdentity("queueViewingActivityTrigger")
+                .withSchedule(scheduleBuilder).build();
     }
 
 }
