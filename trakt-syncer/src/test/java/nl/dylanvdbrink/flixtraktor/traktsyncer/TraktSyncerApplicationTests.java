8796070ae2b4cdb56dd409dfff1cc7469c20b96d
Dylan van den Brink <dylanvandenbrink@live.nl>
2019-09-02 16:10:44 +0200

    Added initial projects

---
+++ trakt-syncer/src/test/java/nl/dylanvdbrink/flixtraktor/traktsyncer/TraktSyncerApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.dylanvdbrink.flixtraktor.traktsyncer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TraktSyncerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
